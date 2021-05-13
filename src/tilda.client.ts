7b4fbcf82c5ff55422f090b708bad687243dff14
Mikhail Monchak <mikhail.monchak.work@gmail.com>
2021-05-13 12:35:56 +0300

    impr: add JSDoc commects

--- src/tilda.client.ts
+++ src/tilda.client.ts
@@ -13,6 +13,9 @@ import fetch from 'cross-fetch';
 export class TildaClient {
   constructor(private readonly publicKey: string, private readonly secretKey: string) {}
 
+  /**
+   * @returns The list of available projects
+   */
   public async getProjectsList(): Promise<TildaProject[]> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getprojectslist/?publickey=${this.publicKey}&secretkey=${this.secretKey}`,
@@ -25,6 +28,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The information about the project
+   */
   public async getProject(projectId: string): Promise<TildaProjectData> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getproject/?publickey=${this.publicKey}&secretkey=${this.secretKey}&projectid=${projectId}`,
@@ -37,6 +43,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The information about the project for export
+   */
   public async getProjectExport(projectId: string): Promise<TildaProjectExport> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getprojectexport/?publickey=${this.publicKey}&secretkey=${this.secretKey}&projectid=${projectId}`,
@@ -49,6 +58,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The list of available pages in the project
+   */
   public async getPagesList(projectId: string): Promise<TildaPage[]> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getpageslist/?publickey=${this.publicKey}&secretkey=${this.secretKey}&projectid=${projectId}`,
@@ -61,6 +73,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The information about the page (+ body html-code)
+   */
   public async getPage(pageId: string): Promise<TildaPageData> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getpage/?publickey=${this.publicKey}&secretkey=${this.secretKey}&pageid=${pageId}`,
@@ -73,6 +88,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The information about the page (+ fullpage html-code)
+   */
   public async getPageFull(pageId: string): Promise<TildaPageData> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getpagefull/?publickey=${this.publicKey}&secretkey=${this.secretKey}&pageid=${pageId}`,
@@ -85,6 +103,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The information about the page for export (+ body html-code)
+   */
   public async getPageExport(pageId: string): Promise<TildaPageExport> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getpageexport/?publickey=${this.publicKey}&secretkey=${this.secretKey}&pageid=${pageId}`,
@@ -97,6 +118,9 @@ export class TildaClient {
     }
   }
 
+  /**
+   * @returns The information about the page for export (+ fullpage html-code)
+   */
   public async getPageFullExport(pageId: string): Promise<TildaPageExport> {
     const res: Response = await fetch(
       `https://api.tildacdn.info/v1/getpagefullexport/?publickey=${this.publicKey}&secretkey=${this.secretKey}&pageid=${pageId}`,
